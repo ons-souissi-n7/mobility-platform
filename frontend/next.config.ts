e3e49783e7b8773b5975902f436a299f9f62206b
ons-souissi-n7 <ons.souissi@toulouse-inp.fr>
2026-07-29 18:03:03 +0200

    fix(ci): fix trivy errors

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     staleTimes: {
       // 0 = pas de router cache côté client pour les pages dynamiques.
